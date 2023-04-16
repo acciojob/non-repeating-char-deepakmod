f5cc5505aec6f2a436c30b5b3298c291a11edc91
Deepak Modanwal <118596712+deepakmod@users.noreply.github.com>
2023-04-16 21:52:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-//your JS code here. If required.
+function firstNonRepeatedChar(str) {
+    // convert string to an array of characters
+    const arr = str.split("");
+    // iterate over each character in the array
+    for (let i = 0; i < arr.length; i++) {
+      // check if the character appears only once in the array
+      if (arr.indexOf(arr[i]) === arr.lastIndexOf(arr[i])) {
+        // if so, return the character
+        return arr[i];
+      }
+    }
+  
+    // if no non-repeated character is found, return null
+    return null;
+}
+
+const input = prompt("Enter a string:");
+const result = firstNonRepeatedChar(input);
+alert(result);
